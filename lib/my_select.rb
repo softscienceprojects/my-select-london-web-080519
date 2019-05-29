79cd91e104b39d8a1104890b239f547cccb951a5
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-29 19:14:57 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,10 +3,10 @@ def my_select(collection)
  i = 0
  my_collect = [] # empty array
  while i < collection.length
-    if yield(collection[i]) == true
-     my_collect << collection[i]
+    if yield(collection[i]) == true # if the yield -- in this case, num.even returns true
+     my_collect << collection[i] # then push the value to your empty array
    end
-     i += 1
+     i += 1 #keep your counter outside your if statement
  end
- my_collect
+ my_collect # return that new array
 end
