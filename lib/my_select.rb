519af2a8d8db3f99c52a3782e2b949a2a1e22704
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-29 19:15:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ # select the items in the collection to which the block returns true
+ i = 0
+ my_collect = [] # empty array
+ while i < collection.length
+    if yield(collection[i]) == true # if the yield -- in this case, num.even returns true
+     my_collect << collection[i] # then push the value to your empty array
+   end
+     i += 1 #keep your counter outside your if statement
+ end
+ my_collect # return that new array
 end
