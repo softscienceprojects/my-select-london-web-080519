92ecce55258cf09a04135fa1b7c4783c15295a7b
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-29 18:54:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,10 @@
 def my_select(collection)
  # select the items in the collection to which the block returns true
  i = 0
-
+ my_collect = [] # empty array
  while i < collection.length
-   yield
+   my_collect << yield(args[i])
    i += 1
  end
- 
+ my_collect
 end
